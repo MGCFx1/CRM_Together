a2516774200035b353c8d296fbdcf86b4c79a192
MGCFx1 <44883463+MGCFx1@users.noreply.github.com>
2024-10-30 13:04:04 +0000

    Login forms

---
+++ CRM system/Login.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace CRM_system
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.login_register = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.login_show = new System.Windows.Forms.CheckBox();
+            this.login_btn = new System.Windows.Forms.Button();
+            this.login_password = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.login_email = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.login_close = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(799, 543);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.login_register);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.login_show);
+            this.panel1.Controls.Add(this.login_btn);
+            this.panel1.Controls.Add(this.login_password);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.login_email);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(232, 117);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(310, 372);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // login_register
+            // 
+            this.login_register.AutoSize = true;
+            this.login_register.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.login_register.Font = new System.Drawing.Font("Poppins", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_register.ForeColor = System.Drawing.Color.Gray;
+            this.login_register.Location = new System.Drawing.Point(159, 328);
+            this.login_register.Name = "login_register";
+            this.login_register.Size = new System.Drawing.Size(77, 19);
+            this.login_register.TabIndex = 8;
+            this.login_register.Text = "Sign up here";
+            this.login_register.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Poppins", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Gray;
+            this.label4.Location = new System.Drawing.Point(40, 328);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 19);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Don\'t have account?";
+            // 
+            // login_show
+            // 
+            this.login_show.AutoSize = true;
+            this.login_show.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.login_show.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_show.ForeColor = System.Drawing.Color.Gray;
+            this.login_show.Location = new System.Drawing.Point(151, 229);
+            this.login_show.Name = "login_show";
+            this.login_show.Size = new System.Drawing.Size(131, 27);
+            this.login_show.TabIndex = 6;
+            this.login_show.Text = "Show Password";
+            this.login_show.UseVisualStyleBackColor = true;
+            this.login_show.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // login_btn
+            // 
+            this.login_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(154)))), ((int)(((byte)(154)))));
+            this.login_btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.login_btn.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_btn.ForeColor = System.Drawing.Color.Black;
+            this.login_btn.Location = new System.Drawing.Point(99, 271);
+            this.login_btn.Name = "login_btn";
+            this.login_btn.Size = new System.Drawing.Size(99, 33);
+            this.login_btn.TabIndex = 5;
+            this.login_btn.Text = "Sign In";
+            this.login_btn.UseVisualStyleBackColor = false;
+            // 
+            // login_password
+            // 
+            this.login_password.Location = new System.Drawing.Point(25, 192);
+            this.login_password.Name = "login_password";
+            this.login_password.Size = new System.Drawing.Size(257, 27);
+            this.login_password.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(22, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 23);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Password";
+            // 
+            // login_email
+            // 
+            this.login_email.Location = new System.Drawing.Point(25, 118);
+            this.login_email.Name = "login_email";
+            this.login_email.Size = new System.Drawing.Size(257, 27);
+            this.login_email.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Email Address";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome Back!";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(232, 58);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(310, 81);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // login_close
+            // 
+            this.login_close.AutoSize = true;
+            this.login_close.BackColor = System.Drawing.Color.White;
+            this.login_close.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.login_close.Location = new System.Drawing.Point(763, 9);
+            this.login_close.Name = "login_close";
+            this.login_close.Size = new System.Drawing.Size(23, 28);
+            this.login_close.TabIndex = 2;
+            this.login_close.Text = "X";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 540);
+            this.Controls.Add(this.login_close);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Login";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Login_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox login_email;
+        private System.Windows.Forms.CheckBox login_show;
+        private System.Windows.Forms.Button login_btn;
+        private System.Windows.Forms.TextBox login_password;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label login_register;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label login_close;
+    }
+}
+
