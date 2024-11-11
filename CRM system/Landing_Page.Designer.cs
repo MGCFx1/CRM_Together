0c262ebba02fa2a281138f7b0309170f4cd68d16
Aby_117 <Abys_cooler@outlook.com>
2024-11-11 23:56:03 +0000

    Adding Back the Project Files

---
+++ CRM system/Landing_Page.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace CRM_system
+{
+    partial class Landing_Page
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Landing_Page));
+            this.Signup_close = new System.Windows.Forms.Label();
+            this.landing_login = new System.Windows.Forms.Button();
+            this.landing_signup = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Signup_close
+            // 
+            this.Signup_close.AutoSize = true;
+            this.Signup_close.BackColor = System.Drawing.Color.White;
+            this.Signup_close.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Signup_close.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Signup_close.Location = new System.Drawing.Point(747, 22);
+            this.Signup_close.Name = "Signup_close";
+            this.Signup_close.Size = new System.Drawing.Size(22, 28);
+            this.Signup_close.TabIndex = 9;
+            this.Signup_close.Text = "X";
+            this.Signup_close.Click += new System.EventHandler(this.Signup_close_Click);
+            // 
+            // landing_login
+            // 
+            this.landing_login.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(154)))), ((int)(((byte)(154)))));
+            this.landing_login.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.landing_login.ForeColor = System.Drawing.Color.White;
+            this.landing_login.Image = global::CRM_system.Properties.Resources.icons8_login_20;
+            this.landing_login.Location = new System.Drawing.Point(45, 321);
+            this.landing_login.Name = "landing_login";
+            this.landing_login.Size = new System.Drawing.Size(154, 44);
+            this.landing_login.TabIndex = 11;
+            this.landing_login.Text = "        LOGIN";
+            this.landing_login.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.landing_login.UseVisualStyleBackColor = false;
+            this.landing_login.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // landing_signup
+            // 
+            this.landing_signup.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(154)))), ((int)(((byte)(154)))));
+            this.landing_signup.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.landing_signup.ForeColor = System.Drawing.Color.White;
+            this.landing_signup.Image = ((System.Drawing.Image)(resources.GetObject("landing_signup.Image")));
+            this.landing_signup.Location = new System.Drawing.Point(227, 321);
+            this.landing_signup.Name = "landing_signup";
+            this.landing_signup.Size = new System.Drawing.Size(154, 44);
+            this.landing_signup.TabIndex = 12;
+            this.landing_signup.Text = "        SIGN UP";
+            this.landing_signup.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.landing_signup.UseVisualStyleBackColor = false;
+            this.landing_signup.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Poppins", 20.25F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(37, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(395, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BEGIN YOUR JOURNEY WITH";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(40, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "TOGETHER CULTURE!";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(39, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(226, 34);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Why together culture?";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Poppins Light", 8F);
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(41, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(325, 96);
+            this.label4.TabIndex = 4;
+            this.label4.Text = resources.GetString("label4.Text");
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(799, 539);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(41, 295);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(102, 23);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Join Us Today!";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Poppins", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(62, 386);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 48);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "300+";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(52, 423);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(103, 28);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "ATTENDEES";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(161, 423);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(115, 28);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "ORGANISERS";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Poppins", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(185, 386);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(70, 48);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "25+";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Poppins", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(282, 386);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(91, 48);
+            this.label12.TabIndex = 20;
+            this.label12.Text = "500+";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(282, 423);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(91, 28);
+            this.label13.TabIndex = 21;
+            this.label13.Text = "MEMBERS";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Poppins", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(287, 386);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(91, 48);
+            this.label14.TabIndex = 20;
+            this.label14.Text = "500+";
+            this.label14.Click += new System.EventHandler(this.label14_Click);
+            // 
+            // Landing_Page
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 540);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.landing_signup);
+            this.Controls.Add(this.landing_login);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Signup_close);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Landing_Page";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Signup_close;
+        private System.Windows.Forms.Button landing_login;
+        private System.Windows.Forms.Button landing_signup;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+    }
+}
---
+++ CRM system/Landing_Page.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRM_system
+{
+    public partial class Landing_Page : Form
+    {
+        public Landing_Page()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Signup_close_Click(object sender, EventArgs e)
+        {
+            // Display a confirmation dialog
+            var result = MessageBox.Show("Are you sure you want to exit?", "Exit Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            // Check the user's response
+            if (result == DialogResult.Yes)
+            {
+                // If the user clicks Yes, close the application
+                Application.Exit();
+            }
+            // If the user clicks No, do nothing and return to the application
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            using (Pen pen = new Pen(Color.Black, 10)) // black colourr, 10 pixels wide
+            {
+                e.Graphics.DrawLine(pen, 10, 10, 200, 10); // Draw line from (10, 10) to (200, 10)
+            }
+        
+    }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Login sForm = new Login();
+            sForm.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SignUp sForm = new SignUp();
+            sForm.Show();
+            this.Hide();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kryptonColorButton1_SelectedColorChanged(object sender, ComponentFactory.Krypton.Toolkit.ColorEventArgs e)
+        {
+
+        }
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kryptonButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
