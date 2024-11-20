bc3452cdecd9c1c6a970bc0c86d1e165eee469c3
MGCFx1 <44883463+MGCFx1@users.noreply.github.com>
2024-11-20 02:09:57 +0000

    Members Requests & Quick actions panel

--- CRM system/Admins_Forms/AdminsPanel.Designer.cs
+++ CRM system/Admins_Forms/AdminsPanel.Designer.cs
@@ -31,27 +31,27 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminsPanel));
             this.Signup_close = new System.Windows.Forms.Label();
             this.adSidebar = new System.Windows.Forms.FlowLayoutPanel();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
             this.panel8 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.panel5 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.adbtDashboard = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.adbtEvents = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.adbtContacts = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
             this.adbtLearning = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
             this.adSidebar.SuspendLayout();
+            this.panel8.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
             this.panel4.SuspendLayout();
-            this.panel8.SuspendLayout();
             this.panel5.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // Signup_close
@@ -81,38 +81,6 @@
             this.adSidebar.Size = new System.Drawing.Size(212, 540);
             this.adSidebar.TabIndex = 12;
             // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.adbtDashboard);
-            this.panel1.Location = new System.Drawing.Point(3, 128);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(211, 52);
-            this.panel1.TabIndex = 13;
-            // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.adbtEvents);
-            this.panel2.Location = new System.Drawing.Point(3, 186);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(211, 52);
-            this.panel2.TabIndex = 15;
-            // 
-            // panel3
-            // 
-            this.panel3.Controls.Add(this.adbtContacts);
-            this.panel3.Location = new System.Drawing.Point(3, 244);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(211, 52);
-            this.panel3.TabIndex = 15;
-            // 
-            // panel4
-            // 
-            this.panel4.Controls.Add(this.adbtLearning);
-            this.panel4.Location = new System.Drawing.Point(3, 302);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(211, 179);
-            this.panel4.TabIndex = 16;
-            // 
             // panel8
             // 
             this.panel8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(19)))), ((int)(((byte)(19)))));
@@ -128,40 +96,29 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Bold);
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(79, 37);
+            this.label1.Location = new System.Drawing.Point(53, 49);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(95, 28);
             this.label1.TabIndex = 1;
             this.label1.Text = "Nav Menu";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Bold);
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(21, 13);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(173, 28);
-            this.label2.TabIndex = 13;
-            this.label2.Text = "Logged in as admin";
-            // 
-            // panel5
-            // 
-            this.panel5.Controls.Add(this.label2);
-            this.panel5.Location = new System.Drawing.Point(3, 487);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(211, 52);
-            this.panel5.TabIndex = 14;
-            // 
             // pictureBox1
             // 
             this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(35, 31);
+            this.pictureBox1.Location = new System.Drawing.Point(9, 43);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(38, 34);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.adbtDashboard);
+            this.panel1.Location = new System.Drawing.Point(3, 128);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(211, 52);
+            this.panel1.TabIndex = 13;
             // 
             // adbtDashboard
             // 
@@ -182,6 +139,14 @@
             this.adbtDashboard.UseVisualStyleBackColor = false;
             this.adbtDashboard.Click += new System.EventHandler(this.adbtDashboard_Click);
             // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.adbtEvents);
+            this.panel2.Location = new System.Drawing.Point(3, 186);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(211, 52);
+            this.panel2.TabIndex = 15;
+            // 
             // adbtEvents
             // 
             this.adbtEvents.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(19)))), ((int)(((byte)(19)))));
@@ -200,6 +165,14 @@
             this.adbtEvents.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.adbtEvents.UseVisualStyleBackColor = false;
             this.adbtEvents.Click += new System.EventHandler(this.adbtEvents_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.adbtContacts);
+            this.panel3.Location = new System.Drawing.Point(3, 244);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(211, 52);
+            this.panel3.TabIndex = 15;
             // 
             // adbtContacts
             // 
@@ -220,6 +193,14 @@
             this.adbtContacts.UseVisualStyleBackColor = false;
             this.adbtContacts.Click += new System.EventHandler(this.adbtContacts_Click);
             // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.adbtLearning);
+            this.panel4.Location = new System.Drawing.Point(3, 302);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(211, 179);
+            this.panel4.TabIndex = 16;
+            // 
             // adbtLearning
             // 
             this.adbtLearning.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(19)))), ((int)(((byte)(19)))));
@@ -239,6 +220,25 @@
             this.adbtLearning.UseVisualStyleBackColor = false;
             this.adbtLearning.Click += new System.EventHandler(this.adbtLearning_Click);
             // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.label2);
+            this.panel5.Location = new System.Drawing.Point(3, 487);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(211, 52);
+            this.panel5.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(21, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 28);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Logged in as admin";
+            // 
             // AdminsPanel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -253,15 +253,15 @@
             this.Text = "AdminsPanel";
             this.Load += new System.EventHandler(this.AdminsPanel_Load);
             this.adSidebar.ResumeLayout(false);
+            this.panel8.ResumeLayout(false);
+            this.panel8.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
             this.panel4.ResumeLayout(false);
-            this.panel8.ResumeLayout(false);
-            this.panel8.PerformLayout();
             this.panel5.ResumeLayout(false);
             this.panel5.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
